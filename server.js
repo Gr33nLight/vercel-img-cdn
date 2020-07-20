33ea3b7cdc37252c7780ab34f3bdf329fb122ec2
Gr33nLight <bdepaula520@gmail.com>
2020-07-21 00:32:49 +0200

    upd custom server

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// server.js
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-const app = express();
-const port = 3001;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-// Middleware
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
 
-// JSON parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/api/ping', (req, res) => {
-  // random endpoint so that the client can call something
-  res.json({ msg: 'pong' });
-});
-
-// start the Express server
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query);
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
 });
